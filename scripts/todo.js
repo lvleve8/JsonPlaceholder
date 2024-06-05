a74e9ea3a3efb6c52febcdab8f53607ffd50c80c
Eric Schwartz <erics273@gmail.com>
2024-06-05 11:22:55 -0400

    finsihed exercise one

--- scripts/todo.js
+++ scripts/todo.js
@@ -2,6 +2,41 @@
 
 window.onload = () => {
 
-    console.log("it be working");
+    let userInput = document.querySelector("#todoId");
+    let theButton = document.querySelector("#theButton");
+
+    theButton.addEventListener("click", (event) => showInfo(event, userInput.value))
+
+}
+
+function showInfo(event, todoId){
+
+    fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
+        .then((response)=>response.json())
+        .then((todo)=>{
+
+            displayTodo(todo)
+
+        })
+        .catch((error)=>console.log("sh*t be broken"));
+
+}
+
+function displayTodo(todo){
+
+    let resultsDiv = document.querySelector("#results");
+
+    if(Array.isArray(todo)){
+        resultsDiv.innerHTML = `
+        <div>Please enter a valid todo id</div>
+    `;
+
+    }else{
+        resultsDiv.innerHTML = `
+        <div>Title: ${todo.title}</div>
+    `;
+
+    }
+   
 
 }
